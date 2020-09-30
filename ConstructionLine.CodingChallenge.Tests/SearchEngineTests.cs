757b754b31d82fb8648b5d1ea06b010bc69382c7
Raj Gurijala - RDC <raj.g@indowestunited.com>
2020-09-30 11:15:11 +0100

    Add project files.

---
+++ ConstructionLine.CodingChallenge.Tests/SearchEngineTests.cs
@@ -0,0 +1,170 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using NUnit.Framework;
+
+namespace ConstructionLine.CodingChallenge.Tests
+{
+    [TestFixture]
+    public class SearchEngineTests : SearchEngineTestsBase
+    {
+
+        #region Filtered Shirts
+
+        [Test]
+        public void Test()
+        {
+            var shirts = new List<Shirt>
+            {
+                new Shirt(Guid.NewGuid(), "Red - Small", Size.Small, Color.Red),
+                new Shirt(Guid.NewGuid(), "Black - Medium", Size.Medium, Color.Black),
+                new Shirt(Guid.NewGuid(), "Blue - Large", Size.Large, Color.Blue)
+            };
+
+            var searchEngine = new SearchEngine(shirts);
+                        
+            var searchOptions = new SearchOptions
+            {
+                Colors = new List<Color> { Color.Red, Color.Blue },
+                Sizes = new List<Size> { Size.Small }
+            };
+
+            var results = searchEngine.Search(searchOptions);
+
+            // Note: this test results are not matching with the 'requirements' as I understood - I can give explanation - Raj
+            //AssertResults(results.Shirts, searchOptions);
+            //AssertSizeCounts(shirts, searchOptions, results.SizeCounts);
+            //AssertColorCounts(shirts, searchOptions, results.ColorCounts);
+
+            Assert.AreEqual(1, results.Shirts.Count);
+            Assert.AreEqual(1, results.SizeCounts.FirstOrDefault(s => s.Size.Name.Equals(Size.Small.Name)).Count);
+            Assert.AreEqual(1, results.ColorCounts.FirstOrDefault(c => c.Color.Name.Equals(Color.Red.Name)).Count);
+        }
+
+        [Test]
+        public void Search_WithNoSearchOptions_ReturnsAllShirts()
+        {
+            var shirts = new List<Shirt>
+            {
+                new Shirt(Guid.NewGuid(), "Red - Small", Size.Small, Color.Red),
+                new Shirt(Guid.NewGuid(), "Black - Medium", Size.Medium, Color.Black),
+                new Shirt(Guid.NewGuid(), "Blue - Large", Size.Large, Color.Blue)
+            };
+
+            var searchEngine = new SearchEngine(shirts);
+
+            var searchOptions = new SearchOptions
+            {
+                
+            };
+
+            var results = searchEngine.Search(searchOptions);
+
+            Assert.AreEqual(shirts.Count, results.Shirts.Count);
+        }
+
+        [Test]
+        public void Search_ReturnsAllShirts()
+        {
+            var shirts = new List<Shirt>
+            {
+                new Shirt(Guid.NewGuid(), "Red - Small", Size.Small, Color.Red),
+                new Shirt(Guid.NewGuid(), "Black - Medium", Size.Medium, Color.Black),
+                new Shirt(Guid.NewGuid(), "Blue - Large", Size.Large, Color.Blue),
+                new Shirt(Guid.NewGuid(), "Red - Medium", Size.Medium, Color.Red)
+            };
+
+            var searchEngine = new SearchEngine(shirts);
+
+            var searchOptions = new SearchOptions
+            {
+                Colors = new List<Color> { Color.Red }
+            };
+
+            var results = searchEngine.Search(searchOptions);
+
+            var expectedShirts = 2;
+            var expectedRedColorCounts = 2;
+            var expectedSmallSizeCounts = 1;
+            var expectedMediumSizeCounts = 1;
+
+            Assert.AreEqual(expectedShirts, results.Shirts.Count);
+            Assert.AreEqual(expectedRedColorCounts, results.ColorCounts.FirstOrDefault(c => c.Color.Name.Equals(Color.Red.Name)).Count);
+            Assert.AreEqual(expectedSmallSizeCounts, results.SizeCounts.FirstOrDefault(c => c.Size.Name.Equals(Size.Small.Name)).Count);
+            Assert.AreEqual(expectedMediumSizeCounts, results.SizeCounts.FirstOrDefault(c => c.Size.Name.Equals(Size.Medium.Name)).Count);
+        }
+
+        #endregion
+
+        #region Total Shirts
+
+        [Test]
+        public void GetTotalShirts_ReturnsAllShirts()
+        {
+            var shirts = new List<Shirt>
+            {
+                new Shirt(Guid.NewGuid(), "Red - Small", Size.Small, Color.Red),
+                new Shirt(Guid.NewGuid(), "Black - Medium", Size.Medium, Color.Black),
+                new Shirt(Guid.NewGuid(), "Blue - Large", Size.Large, Color.Blue)
+            };
+
+            var searchEngine = new SearchEngine(shirts);
+
+            var actualNumberOfShirts = searchEngine.GetTotalShirts();
+
+            Assert.AreEqual(shirts.Count, actualNumberOfShirts.Count);
+        }
+
+        [Test]
+        public void GetTotalShirtsByColor_Returns1Red1Black1Blue0WhiteAnd0YellowShirts()
+        {
+            var shirts = new List<Shirt>
+            {
+                new Shirt(Guid.NewGuid(), "Red - Small", Size.Small, Color.Red),
+                new Shirt(Guid.NewGuid(), "Black - Medium", Size.Medium, Color.Black),
+                new Shirt(Guid.NewGuid(), "Blue - Large", Size.Large, Color.Blue)
+            };
+
+            var searchEngine = new SearchEngine(shirts);
+
+            var expectedRedShirts = 1;
+            var expectedBlackShirts = 1;
+            var expectedBlueShirts = 1;
+            var expectedWhiteShirts = 0;
+            var expectedYellowShirts = 0;
+
+            Dictionary<string, int> totalShirtsByColor = searchEngine.GetTotalShirtsByColor();
+
+            Assert.AreEqual(expectedRedShirts, totalShirtsByColor[Color.Red.Name]);
+            Assert.AreEqual(expectedBlackShirts, totalShirtsByColor[Color.Black.Name]);
+            Assert.AreEqual(expectedBlueShirts, totalShirtsByColor[Color.Blue.Name]);
+            Assert.AreEqual(expectedWhiteShirts, totalShirtsByColor[Color.White.Name]);
+            Assert.AreEqual(expectedYellowShirts, totalShirtsByColor[Color.Yellow.Name]);
+        }
+
+        [Test]
+        public void GetTotalShirtsBySize_Returns1Small1MediumAnd1LargeShirts()
+        {
+            var shirts = new List<Shirt>
+            {
+                new Shirt(Guid.NewGuid(), "Red - Small", Size.Small, Color.Red),
+                new Shirt(Guid.NewGuid(), "Black - Medium", Size.Medium, Color.Black),
+                new Shirt(Guid.NewGuid(), "Blue - Large", Size.Large, Color.Blue)
+            };
+
+            var searchEngine = new SearchEngine(shirts);
+
+            var expectedSmallShirts = 1;
+            var expectedMediumShirts = 1;
+            var expectedLargeShirts = 1;
+            
+            Dictionary<string, int> totalShirtsBySize = searchEngine.GetTotalShirtsBySize();
+
+            Assert.AreEqual(expectedSmallShirts, totalShirtsBySize[Size.Small.Name]);
+            Assert.AreEqual(expectedMediumShirts, totalShirtsBySize[Size.Medium.Name]);
+            Assert.AreEqual(expectedLargeShirts, totalShirtsBySize[Size.Large.Name]);
+        }
+
+        #endregion
+    }
+}
---
+++ ConstructionLine.CodingChallenge/SearchEngine.cs
@@ -0,0 +1,198 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace ConstructionLine.CodingChallenge
+{
+    public class SearchEngine
+    {
+        private readonly List<Shirt> _shirts;
+        private List<ColorCount> _colorCounts;
+        private List<SizeCount> _sizeCounts;
+
+        public Dictionary<string, int> _totalShirtsByColor;
+        public Dictionary<string, int> _totalShirtsBySize;
+
+        //public List<Shirt> FilteredShirts;
+        //public Dictionary<string, int> FilteredShirtsByColor;
+        //public Dictionary<string, int> FilteredShirtsBySize;
+
+        public SearchEngine(List<Shirt> shirts)
+        {
+            _shirts = shirts;
+
+            _totalShirtsByColor = PopulateTotalShirtsByColor();
+            _totalShirtsBySize = PopulateTotalShirtsBySize();
+
+            // TODO: data preparation and initialisation of additional data structures to improve performance goes here.
+
+            _colorCounts = GetDefaultColorCounts();
+            _sizeCounts = GetDefaultSizeCounts();
+        }
+
+        #region Public Methods
+
+        #region Total Shirts
+
+        public List<Shirt> GetTotalShirts()
+        {
+            return _shirts;
+        }
+
+        public Dictionary<string, int> GetTotalShirtsByColor()
+        {
+            return _totalShirtsByColor;
+        }
+
+        public Dictionary<string, int> GetTotalShirtsBySize()
+        {
+            return _totalShirtsBySize;
+        }
+
+        #endregion
+
+        #region Filtered Shirts
+
+        public SearchResults Search(SearchOptions options)
+        {
+            //PopulateColorCounts(options);
+            //PopulateSizeCounts(options);
+
+            var sizeIds = options.Sizes.Select(s => s.Id).ToList();
+            var colorIds = options.Colors.Select(c => c.Id).ToList();
+
+            var shirts = _shirts.Where(s => (!sizeIds.Any() || sizeIds.Contains(s.Size.Id)) && (!colorIds.Any() || colorIds.Contains(s.Color.Id))).ToList();
+
+            foreach (var shirt in shirts)
+            {
+                AddToColorCounts(shirt);
+                AddToSizeCounts(shirt);
+            }
+
+            return new SearchResults
+            {
+                Shirts = shirts,
+                SizeCounts = _sizeCounts,
+                ColorCounts = _colorCounts
+            };
+        }
+
+        #endregion
+
+        #endregion
+
+        #region Private Methods
+
+        private Dictionary<string, int> PopulateTotalShirtsByColor()
+        {
+            var dictionary = new Dictionary<string, int>();
+
+            foreach (var color in Color.All)
+            {
+                dictionary.Add(color.Name, _shirts.Count(s => s.Color.Name.Equals(color.Name)));
+            }
+
+            return dictionary;
+        }
+
+        private Dictionary<string, int> PopulateTotalShirtsBySize()
+        {
+            var dictionary = new Dictionary<string, int>();
+
+            foreach (var size in Size.All)
+            {
+                dictionary.Add(size.Name, _shirts.Count(s => s.Size.Name.Equals(size.Name)));
+            }
+
+            return dictionary;
+        }
+
+        private void AddToSizeCounts(Shirt shirt)
+        {
+            SizeCount sizeCount = _sizeCounts.FirstOrDefault(s => s.Size.Id == shirt.Size.Id);
+
+            if (sizeCount != null)
+            {
+                sizeCount.Count += 1;
+            }
+        }
+
+        private void AddToColorCounts(Shirt shirt)
+        {
+            ColorCount colorCount = _colorCounts.FirstOrDefault(s => s.Color.Id == shirt.Color.Id);
+
+            if (colorCount != null)
+            {
+                colorCount.Count += 1;
+            }
+        }
+
+        //private void PopulateColorCounts(SearchOptions options)
+        //{
+        //    var message = string.Empty;
+
+        //    //foreach (var color in options.Colors)
+        //    foreach (var color in Color.All)
+        //    {
+        //        List<Shirt> shirts = _shirts.Where(s => s.Color.Id == color.Id).ToList();
+
+        //        message += $"{color.Name} : {shirts.Count} {Environment.NewLine}";
+
+        //        if (shirts.Any())
+        //        {
+        //            ColorCount colorCount = _colorCounts.FirstOrDefault(s => s.Color.Id == color.Id);
+
+        //            if (colorCount != null)
+        //            {
+        //                colorCount.Count += shirts.Count;
+        //            }
+        //        }
+        //    }
+        //}
+
+        //private void PopulateSizeCounts(SearchOptions options)
+        //{
+        //    //foreach (var size in options.Sizes)
+        //    foreach (var size in Size.All)
+        //    {
+        //        List<Shirt> shirts = _shirts.Where(s => s.Size.Id == size.Id).ToList();
+
+        //        if (shirts.Any())
+        //        {
+        //            SizeCount sizeCount = _sizeCounts.FirstOrDefault(s => s.Size.Id == size.Id);
+
+        //            if (sizeCount != null)
+        //            {
+        //                sizeCount.Count += shirts.Count;
+        //            }
+        //        }
+        //    }
+        //}
+
+        private List<ColorCount> GetDefaultColorCounts()
+        {
+            var colorCounts = new List<ColorCount>();
+
+            foreach (var color in Color.All)
+            {
+                colorCounts.Add(new ColorCount { Color = color, Count = 0 });
+            }
+
+            return colorCounts;
+        }
+
+        private List<SizeCount> GetDefaultSizeCounts()
+        {
+            var sizeCounts = new List<SizeCount>();
+
+            foreach (var size in Size.All)
+            {
+                sizeCounts.Add(new SizeCount { Size = size, Count = 0 });
+            }
+
+            return sizeCounts;
+        }
+
+        #endregion
+    }
+}
